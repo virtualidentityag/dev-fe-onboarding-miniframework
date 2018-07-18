471c6f79e21d19df10383e13ed3e73a432f00cad
Lukas Müller <lukas.mueller@virtual-identity.com>
2018-07-18 11:36:26 +0200

    added bugs, JS file deployment and cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const connect = require('gulp-connect');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function() {
   gulp
@@ -11,12 +12,17 @@ gulp.task('sass', function() {
         outputStyle: 'expanded'
       })
     )
-    .pipe(gulp.dest('./srv/css'));
+    .pipe(gulp.dest('./srv/ssc'));
 });
 
-gulp.task('copy', function() {
-  gulp.src('./src/index.html').pipe(gulp.dest('./srv/'));
+gulp.task('js', function(){
+  gulp.src(['./src/js/*.js']).pipe(gulp.dest('./srv/js/'));
+  gulp.src('./node_modules/es6-scroll-to/lib/index.js').pipe(rename('es6-scroll-to.js')).pipe(gulp.dest('./srv/js/'));
 });
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html').pipe(gulp.dest('./srv/'));
+})
 
 gulp.task('connect:open', function() {
   const opn = require('opn');
@@ -36,9 +42,10 @@ gulp.task('livereload', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/*.html', ['copy']);
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/js/*.js', ['js']);
   gulp.watch('./src/scss/*.scss', ['sass']);
   gulp.watch('./src/**/*', ['livereload']);
 });
 
-gulp.task('default', ['connect', 'connect:open', 'watch', 'sass', 'copy']);
+gulp.task('default', ['connect', 'connect:open', 'watch', 'sass', 'js', 'html']);
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import {animatedScrollTo} from './es6-scroll-to.js';
+
+document.querySelector('.title').innerHTML = 'Great success!'
+document.querySelector('.text').innerHTML = 'Javascript is working!'
+
+animatedScrollTo(500);
