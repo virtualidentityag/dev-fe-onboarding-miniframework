87efd2fa7394fec7f35ac43d1f269c29ef35c113
Ivaylo Stoyanov <ivaylo.stoyanov@virtual-identity.com>
2018-10-26 18:47:37 +0200

    Contact us div inserted

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
-import {animatedScrollTo} from './es6-scroll-to.js';
-
+// import {animatedScrollTo} from './es6-scroll-to.js';
 // document.querySelector('.title').innerHTML = 'Great success!'
 // document.querySelector('.text').innerHTML = 'Javascript is working!'
-
 // animatedScrollTo(500);
+
+
+
+var ctaSectionContact = document.getElementById ("ctaSectionContactUs");
+ctaSectionContact.insertAdjacentHTML('afterend', '<div class="contactUs"><span>Contact Us</span></div>')
+
+
