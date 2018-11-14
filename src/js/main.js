d3efb276e53f40b4b358c600773a190bb4b516e9
Ivaylo Stoyanov <ivaylo.stoyanov@virtual-identity.com>
2018-11-14 16:10:27 +0100

    Functionality added

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,28 @@
-// import {animatedScrollTo} from './es6-scroll-to.js';
-// document.querySelector('.title').innerHTML = 'Great success!'
-// document.querySelector('.text').innerHTML = 'Javascript is working!'
-// animatedScrollTo(500);
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(btnClick => btnClick.addEventListener('click', openInfobox));
 
 
+function openInfobox(event) {
+    const btn = event.target;
+    const elementBlockSelector = 'contactUs';
+    const open = '--opened';
+    const allContactUs = document.querySelectorAll('.contactUs');
+    const ctaSection = '.ctaSection';
 
-var ctaSectionContact = document.getElementById ("ctaSectionContactUs");
-ctaSectionContact.insertAdjacentHTML('afterend', '<div class="contactUs"><span>Contact Us</span></div>')
 
+    for (let i = 0; i < allContactUs.length; i++) {
+        const element = allContactUs[i];
 
+        if (btn.closest(ctaSection).querySelector('.contactUs') !== element) {
+            element.classList.remove(elementBlockSelector + open);
+        }
+    };
+    // ---ES6 way of doing the loop---
+    // console.log(allContactUs.forEach);
+    // allContactUs.forEach(function(element){
+    //     console.log(element);
+    //     element.classList.remove(elementBlockSelector + open);
+    // });
+
+    btn.closest(ctaSection).querySelector('.contactUs').classList.toggle(elementBlockSelector + open);
+};
