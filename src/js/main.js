9659a6a04526414173d0279d0c82779f71d3e029
simonbraun <simon.braun@virtual-identity.com>
2019-04-08 12:39:13 +0200

    js optimazitations

--- src/js/main.js
+++ src/js/main.js
@@ -12,10 +12,32 @@ document.addEventListener('DOMContentLoaded', function(){
     // your code goes here
     console.log(document.querySelector(".textBox__linkButton--showInfo"));
     console.log("page loaded");
+    console.log(document.getElementsByClassName("textBox__linkButton--showInfo"));
 
-    document.getElementsByClassName("textBox__linkButton--showInfo")[0].onclick=function(){
-        console.log("clicked");
-        document.getElementsByClassName("tooltip")[0].classList.toggle("tooltip--show");
-    };
+    var linkButtons = Array.prototype.slice.call(document.getElementsByClassName("textBox__linkButton--showInfo"));
+
+linkButtons.forEach((linkButton, index)=>{
+
+  linkButton.onclick=()=>{
+    console.log("clicked");
+    document.getElementsByClassName("tooltip")[index].classList.toggle("tooltip--show");
+  };
+});
+
+    // document.getElementsByClassName("textBox__linkButton--showInfo")[0].onclick=function(){
+    //     console.log("clicked");
+    //     document.getElementsByClassName("tooltip")[0].classList.toggle("tooltip--show");
+    // };
+    //
+    // document.getElementsByClassName("textBox__linkButton--showInfo")[1].onclick=function(){
+    //     console.log("clicked");
+    //     document.getElementsByClassName("tooltip")[1].classList.toggle("tooltip--show");
+    // };
+    //
+    // document.getElementsByClassName("textBox__linkButton--showInfo")[2].onclick=function(){
+    //     console.log("clicked");
+    //     document.getElementsByClassName("tooltip")[2].classList.toggle("tooltip--show");
+    // };
+
 }, false);
 animatedScrollTo(500);
