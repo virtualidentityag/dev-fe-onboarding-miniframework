f2f776670df09c16fc78803c160b1b0abdaae972
simonbraun <simon.braun@virtual-identity.com>
2019-04-10 16:46:23 +0200

    js optimization, changed naming in HTML and CSS

--- src/js/main.js
+++ src/js/main.js
@@ -10,34 +10,26 @@ import {animatedScrollTo} from './es6-scroll-to.js';
 // without jQuery (doesn't work in older IEs)
 document.addEventListener('DOMContentLoaded', function(){
     // your code goes here
-    console.log(document.querySelector(".textBox__linkButton--showInfo"));
+    console.log(document.querySelector(".imageText__linkButton--showInfo"));
     console.log("page loaded");
-    console.log(document.getElementsByClassName("textBox__linkButton--showInfo"));
+    console.log(document.getElementsByClassName("imageText__linkButton--showInfo"));
 
-    var linkButtons = Array.prototype.slice.call(document.getElementsByClassName("textBox__linkButton--showInfo"));
+    var linkButtons = [].slice.call(document.querySelectorAll(".imageText__linkButton--showInfo"));
+    console.log(linkButtons);
+linkButtons.forEach((linkButton) =>{
+  console.log("i am a button");
 
-linkButtons.forEach((linkButton, index)=>{
-
-  linkButton.onclick=()=>{
+linkButton.addEventListener("click", (e) =>{
     console.log("clicked");
-    document.getElementsByClassName("tooltip")[index].classList.toggle("tooltip--show");
-  };
+    var tooltipToToggle = e.target.parentNode.parentNode.querySelector(".imageText__tooltip");
+    document.querySelectorAll(".imageText__tooltip--show").forEach((tooltipOpened) => {
+if (tooltipToToggle != tooltipOpened){
+    tooltipOpened.classList.remove("imageText__tooltip--show");
+}
+    });
+    tooltipToToggle.classList.toggle("imageText__tooltip--show")
+  });
 });
-
-    // document.getElementsByClassName("textBox__linkButton--showInfo")[0].onclick=function(){
-    //     console.log("clicked");
-    //     document.getElementsByClassName("tooltip")[0].classList.toggle("tooltip--show");
-    // };
-    //
-    // document.getElementsByClassName("textBox__linkButton--showInfo")[1].onclick=function(){
-    //     console.log("clicked");
-    //     document.getElementsByClassName("tooltip")[1].classList.toggle("tooltip--show");
-    // };
-    //
-    // document.getElementsByClassName("textBox__linkButton--showInfo")[2].onclick=function(){
-    //     console.log("clicked");
-    //     document.getElementsByClassName("tooltip")[2].classList.toggle("tooltip--show");
-    // };
 
 }, false);
 animatedScrollTo(500);
