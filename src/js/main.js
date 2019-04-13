c923be911b908b7317227c28a11623cf57b70eb6
simonbraun <simon.braun@virtual-identity.com>
2019-04-13 14:39:49 +0200

    added content, added styling, added first tooltip interaction in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
-import {animatedScrollTo} from './es6-scroll-to.js';
+/* eslint semi: "error" */
 
-document.querySelector('.title').innerHTML = 'Great success!'
-document.querySelector('.text').innerHTML = 'Javascript is working!'
+import { animatedScrollTo } from './es6-scroll-to.js';
 
-animatedScrollTo(500);
+animatedScrollTo(50);
+
+var toggleButtons = [].slice.call(document.querySelectorAll('.callToAction__togglebutton'));
+
+console.log(toggleButtons);
+
+toggleButtons.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    var tooltipClosed = e.target.parentNode.parentNode.querySelector('.callToAction__tooltip');
+    document.querySelectorAll('.callToAction__tooltip--show').forEach((tooltipOpen) => {
+      if (tooltipOpen !== tooltipClosed) {
+        tooltipOpen.classList.replace('callToAction__tooltip--show', 'callToAction__tooltip');
+      };
+    });
+    tooltipClosed.classList.toggle('callToAction__tooltip--show');
+  });
+});
