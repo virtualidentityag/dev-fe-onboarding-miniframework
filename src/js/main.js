033e2a831ba9758128f5bbc26f54a76ae3cfe4c1
simonbraun <simon.braun@virtual-identity.com>
2019-04-15 15:59:27 +0200

    added third conversion

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,25 @@ var toggleButtons = [].slice.call(document.querySelectorAll('.callToAction__togg
 
 console.log(toggleButtons);
 
+var imageContainer = document.querySelector('.callToAction--thirdConversion').querySelector('.callToAction__backgroundImage');
+console.log(imageContainer);
+
 toggleButtons.forEach((element) => {
   element.addEventListener('click', (e) => {
-    var tooltipClosed = e.target.parentNode.parentNode.querySelector('.callToAction__tooltip');
+    var tooltipClicked = e.target.parentNode.parentNode.querySelector('.callToAction__tooltip');
+    var imageFlat = e.target.parentNode.parentNode.querySelector('.callToAction__image');
     document.querySelectorAll('.callToAction__tooltip--show').forEach((tooltipOpen) => {
-      if (tooltipOpen !== tooltipClosed) {
-        tooltipOpen.classList.replace('callToAction__tooltip--show', 'callToAction__tooltip');
+      if (tooltipOpen !== tooltipClicked) {
+        tooltipOpen.classList.remove('callToAction__tooltip--show');
       };
     });
-    tooltipClosed.classList.toggle('callToAction__tooltip--show');
+    document.querySelectorAll('.callToAction_image--round').forEach((imageRound) => {
+      if (imageRound !== imageFlat) {
+        imageRound.classList.remove('callToAction__image--round');
+      };
+    });
+    tooltipClicked.classList.toggle('callToAction__tooltip--show');
+    imageFlat.classList.toggle('callToAction__image--round');
+    imageContainer.classList.toggle('callToAction__backgroundImage--round');
   });
 });
