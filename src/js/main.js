afef2cb01c10bde85eafa351fae61cea77925afe
simonbraun <simon.braun@virtual-identity.com>
2019-04-16 12:04:37 +0200

    refactored the scss and js  structure

--- src/js/main.js
+++ src/js/main.js
@@ -8,25 +8,14 @@ var toggleButtons = [].slice.call(document.querySelectorAll('.callToAction__togg
 
 console.log(toggleButtons);
 
-var imageContainer = document.querySelector('.callToAction--thirdConversion').querySelector('.callToAction__backgroundImage');
-console.log(imageContainer);
-
 toggleButtons.forEach((element) => {
   element.addEventListener('click', (e) => {
-    var tooltipClicked = e.target.parentNode.parentNode.querySelector('.callToAction__tooltip');
-    var imageFlat = e.target.parentNode.parentNode.querySelector('.callToAction__image');
-    document.querySelectorAll('.callToAction__tooltip--show').forEach((tooltipOpen) => {
-      if (tooltipOpen !== tooltipClicked) {
-        tooltipOpen.classList.remove('callToAction__tooltip--show');
-      };
-    });
-    document.querySelectorAll('.callToAction_image--round').forEach((imageRound) => {
-      if (imageRound !== imageFlat) {
-        imageRound.classList.remove('callToAction__image--round');
-      };
-    });
-    tooltipClicked.classList.toggle('callToAction__tooltip--show');
-    imageFlat.classList.toggle('callToAction__image--round');
-    imageContainer.classList.toggle('callToAction__backgroundImage--round');
+    var callToActionClicked = e.target.parentNode.parentNode;
+    console.log(callToActionClicked);
+    var callToActionWithTooltipOpen = document.querySelector('.callToAction--tooltipOpen');
+     if (callToActionWithTooltipOpen && (callToActionClicked !== callToActionWithTooltipOpen)) {
+        callToActionWithTooltipOpen.classList.remove('callToAction--tooltipOpen');
+    };
+    callToActionClicked.classList.toggle('callToAction--tooltipOpen');
   });
 });
