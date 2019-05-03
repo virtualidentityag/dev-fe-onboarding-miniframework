c2edcfa26b683b9d1e032c688e183f80298d2d02
simonbraun <simon.braun@virtual-identity.com>
2019-05-03 16:16:38 +0200

    same tooltip functionality but with use of JQuery + fixed bugs in JS

--- src/js/JQuery.js
+++ src/js/JQuery.js
@@ -7,36 +7,14 @@ animatedScrollTo(50);
 var toggleButtons = [].slice.call(jQuery("body").find(".callToAction__togglebutton"));
 console.log(toggleButtons);
 
-$.each(toggleButtons, function() {
-  console.log("huhu00");
-  $(this).click(function() {
-    console.log("clickclick");
-    
-  })
-});
-
 $(".callToAction__togglebutton").each(function() {
-  console.log("huhu11");
+
   $(this).on("click", function() {
     var callToActionClicked = $(this).parent().parent();
-    console.log("clicked" + callToActionClicked); 
     var callToActionWithTooltipOpen = $(".callToAction--tooltipOpen");  
+    callToActionClicked.toggleClass("callToAction--tooltipOpen");
     if (callToActionWithTooltipOpen && (callToActionClicked !== callToActionWithTooltipOpen)) {
       callToActionWithTooltipOpen.removeClass("callToAction--tooltipOpen");
-    };
-    callToActionClicked.toggleClass("callToAction--tooltipOpen");
+    }; 
   });
 });
-
-// toggleButtons.forEach((element) => {
-//   element.addEventListener('click', (e) => {
-//     var callToActionClicked = e.target.parentNode.parentNode;
-//     console.log(callToActionClicked);
-//     var callToActionWithTooltipOpen = document.querySelector('.callToAction--tooltipOpen');
-//     if (callToActionWithTooltipOpen && (callToActionClicked !== callToActionWithTooltipOpen)) {
-//       callToActionWithTooltipOpen.classList.remove('callToAction--tooltipOpen');
-//     };
-//     callToActionClicked.classList.toggle('callToAction--tooltipOpen');
-//   });
-// });
-
