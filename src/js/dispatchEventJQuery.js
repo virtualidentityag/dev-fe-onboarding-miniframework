6c2df9384923dc59efe5ec2424b34218d749da69
simonbraun <simon.braun@virtual-identity.com>
2019-05-07 17:30:28 +0200

    refactor(cleanup): cleanup
    in dispatchEventJQuery.js added a second object to the custom event, and
    consoled it for better understanding

--- src/js/dispatchEventJQuery.js
+++ src/js/dispatchEventJQuery.js
@@ -2,7 +2,12 @@ $(".callToAction__togglebutton").each((index, button) => {
     $(button).on('click', function (clickevent) {
         $(".callToAction").each(function (index, mainComponent) {
             $(mainComponent).trigger('closeOthers', [{
-                origin: $(button).parent().parent()
+                origin: $(button).parent().parent(),
+                object: {
+                    0: "hallo",
+                    1: "test",
+                    test: 123
+                }
             }]);
         })
     })
@@ -10,6 +15,7 @@ $(".callToAction__togglebutton").each((index, button) => {
 
 $(".callToAction").each((index, component) => {
     $(component).on('closeOthers', function (event, data) {
+        console.log(data.object[1]); 
         if (data.origin[0] == component) {
             $(component).toggleClass("callToAction--tooltipOpen")
         } else {
