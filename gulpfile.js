243f84921ac4732a10f5d11616cb2e3fdbefd6f6
Christoph Saile <christoph.saile@virtual-identity.com>
2019-09-24 14:57:14 +0200

    fix gulpfile.js for gulp v4+

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,74 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const connect = require('gulp-connect');
-var rename = require('gulp-rename');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const connect = require("gulp-connect");
+const merge = require("merge-stream");
+const rename = require("gulp-rename");
 
-gulp.task('sass', function() {
-  gulp
-    .src(['./src/scss/*.scss'])
-    .pipe(
-      sass({
-        includePaths: ['./src/scss'],
-        outputStyle: 'expanded'
-      })
-    )
-    .pipe(gulp.dest('./srv/ssc'));
+gulp.task("sass", function() {
+  const normalize = gulp.src("node_modules/normalize.css/normalize.css");
+  const scss = gulp.src(["./src/scss/*.scss"]).pipe(
+    sass({
+      includePaths: ["./src/scss"],
+      outputStyle: "expanded"
+    })
+  );
+
+  return merge(normalize, scss).pipe(gulp.dest("./srv/css"));
 });
 
-gulp.task('js', function(){
-  gulp.src(['./src/js/*.js']).pipe(gulp.dest('./srv/js/'));
-  gulp.src('./node_modules/es6-scroll-to/lib/index.js').pipe(rename('es6-scroll-to.js')).pipe(gulp.dest('./srv/js/'));
+gulp.task("js", function() {
+  gulp.src(["./src/js/*.js"]).pipe(gulp.dest("./srv/js/"));
+  return gulp
+    .src("./node_modules/es6-scroll-to/lib/index.js")
+    .pipe(rename("es6-scroll-to.js"))
+    .pipe(gulp.dest("./srv/js/"));
 });
 
-gulp.task('html', function () {
-  gulp.src('./src/*.html').pipe(gulp.dest('./srv/'));
-})
-
-gulp.task('connect:open', function() {
-  const opn = require('opn');
-  return opn('http://localhost:1337');
+gulp.task("html", function() {
+  return gulp.src("./src/*.html").pipe(gulp.dest("./srv/"));
 });
 
-gulp.task('connect', function() {
+gulp.task("connect:open", function(done) {
+  const opn = require("opn");
+  opn("http://localhost:1337");
+  done();
+});
+
+gulp.task("connect", function(done) {
   connect.server({
-    root: 'srv',
+    root: "srv",
     port: 1337,
     livereload: true
   });
+  done();
 });
 
-gulp.task('livereload', function() {
-  gulp.src('./srv/**/*').pipe(connect.reload());
+gulp.task("livereload", function() {
+  return gulp.src("./srv/**/*").pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./src/*.html', ['html']);
-  gulp.watch('./src/js/*.js', ['js']);
-  gulp.watch('./src/scss/*.scss', ['sass']);
-  gulp.watch('./src/**/*', ['livereload']);
+gulp.task("assets", function() {
+  return gulp.src("./src/assets/*.*").pipe(gulp.dest("./srv/assets/"));
 });
 
-gulp.task('default', ['connect', 'connect:open', 'watch', 'sass', 'js', 'html']);
+gulp.task("watch", function(done) {
+  gulp.watch("./src/*.html", gulp.parallel("html"));
+  gulp.watch("./src/js/*.js", gulp.parallel("js"));
+  gulp.watch("./src/scss/*.scss", gulp.parallel("sass"));
+  gulp.watch("./src/**/*", gulp.parallel("livereload"));
+  gulp.watch("./src/assets/*.*", gulp.parallel("assets"));
+  done();
+});
+
+gulp.task(
+  "default",
+  gulp.parallel(
+    "connect",
+    "connect:open",
+    "watch",
+    "sass",
+    "js",
+    "html",
+    "assets"
+  )
+);
